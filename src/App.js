1cdc2a354b503de59fd14da326793f1e1f3d5558
Nunikid <nunithefanoftheyogscast@gmail.com>
2024-10-22 23:05:07 -0600

    cambio a español y unos logos

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ function App() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             SecurePro
           </Typography>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/products">Products & Services</Button>
-          <Button color="inherit" component={Link} to="/contact">Contact</Button>
+          <Button color="inherit" component={Link} to="/">Inicio</Button>
+          <Button color="inherit" component={Link} to="/products">Productos y Servicios</Button>
+          <Button color="inherit" component={Link} to="/contact">Contacto</Button>
         </Toolbar>
       </AppBar>
       <Routes>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,49 +1,32 @@
 import React from 'react';
-import { Container, Typography, Grid, Card, CardContent, CardActionArea } from '@mui/material';
+import { Container, Typography, Grid, Card, CardContent, CardActionArea, Divider, Box } from '@mui/material';
 import GppGoodIcon from '@mui/icons-material/GppGood';
 import EngineeringIcon from '@mui/icons-material/Engineering';
-import SecurityIcon from '@mui/icons-material/Security';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import GradingIcon from '@mui/icons-material/Grading';
 
 const Products = () => {
   return (
-    <Container >
+    <Container>
       <Typography variant="h4" gutterBottom my={4} style={{ textAlign: 'center' }}>
-        Our Products & Services
+        Nuestros Productos y Servicios
       </Typography>
       <Grid container spacing={4}>
         <Grid item xs={12} md={4}>
           <Card>
             <CardActionArea>
               <CardContent style={{ textAlign: 'center' }}>
-                <GppGoodIcon fontSize="large" style={{ fontSize: 50 }}/>
-                <Typography variant="h6" style={{marginTop: '10px'}}>
-                  Managed Security Services
+                <GppGoodIcon fontSize="large" style={{ fontSize: 50 }} />
+                <Divider style={{ margin: '20px 0' }} />
+                <Typography variant="h6" style={{ marginTop: '10px' }}>
+                  Servicios de Seguridad Gestionados
                 </Typography>
-                <Typography variant="body2" style={{marginTop: '20px'}}>
-                  Continuous network monitoring, incident response, and vulnerability management.
+                <Typography variant="body2" style={{ marginTop: '20px' }}>
+                  Monitoreo continuo de la red, respuesta a incidentes y gestión de vulnerabilidades.
                 </Typography>
-                <Typography variant="body1" color="textSecondary" style={{marginTop: '20px'}}>
-                  Price: $500 MXN/month
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardActionArea>
-              <CardContent style={{ textAlign: 'center' }}>
-                <EngineeringIcon fontSize="large" style={{ fontSize: 50 }}/>
-                <Typography variant="h6" style={{marginTop: '10px'}}>
-                  Corrective Maintenance
-                </Typography>
-                <Typography variant="body2" style={{marginTop: '20px'}}>
-                  Help recover computer & devices affected by a cyberattack.
-                </Typography>
-                <Typography variant="body1" color="textSecondary" style={{marginTop: '20px'}}>
-                  Price: $1000 MXN/hour
+                <Typography variant="body1" color="textSecondary" style={{ marginTop: '20px' }}>
+                  Precio: $500 MXN/mes
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -53,15 +36,35 @@ const Products = () => {
           <Card>
             <CardActionArea>
               <CardContent style={{ textAlign: 'center' }}>
-              <SecurityIcon fontSize="large" style={{ fontSize: 50 }}/>
-                <Typography variant="h6" style={{marginTop: '10px'}}>
-                  Security Audits
+                <EngineeringIcon fontSize="large" style={{ fontSize: 50 }} />
+                <Divider style={{ margin: '20px 0' }} />
+                <Typography variant="h6" style={{ marginTop: '10px' }}>
+                  Mantenimiento Correctivo
                 </Typography>
-                <Typography variant="body2" style={{marginTop: '20px'}}>
-                  Comprehensive review of your organization's cybersecurity policies.
+                <Typography variant="body2" style={{ marginTop: '20px' }}>
+                  Ayuda a recuperar computadoras y dispositivos afectados por un ciberataque.
                 </Typography>
-                <Typography variant="body1" color="textSecondary" style={{marginTop: '20px'}}>
-                  Price: $1200 MXN/audit
+                <Typography variant="body1" color="textSecondary" style={{ marginTop: '20px' }}>
+                  Precio: $1000 MXN/hora
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card>
+            <CardActionArea>
+              <CardContent style={{ textAlign: 'center' }}>
+                <GradingIcon fontSize="large" style={{ fontSize: 50 }} />
+                <Divider style={{ margin: '20px 0' }} />
+                <Typography variant="h6" style={{ marginTop: '10px' }}>
+                  Auditorías de Seguridad
+                </Typography>
+                <Typography variant="body2" style={{ marginTop: '20px' }}>
+                  Revisión comprensiva y exhaustiva sobre las políticas actuales de seguridad de la compañía.
+                </Typography>
+                <Typography variant="body1" color="textSecondary" style={{ marginTop: '20px' }}>
+                  Precio: $1200 MXN/auditoría
                 </Typography>
               </CardContent>
             </CardActionArea>
@@ -69,17 +72,53 @@ const Products = () => {
         </Grid>
       </Grid>
       <Typography variant="h5" gutterBottom style={{ marginTop: '60px', textAlign: 'center' }}>
-        Payment & Shipping Methods
+        Métodos de Pago y Envío
       </Typography>
       <Typography variant="body1" paragraph>
-        We accept payments via credit card, PayPal, and wire transfers. Products are delivered digitally, and no shipping is required. Services depends on how far away the location that needs asistance is.
+        Aceptamos pagos con tarjeta de crédito, PayPal y transferencias bancarias. 
+        <br/> Los productos se entregan de forma digital, por lo que no se requiere envío. 
+        <br/> El tiempo de respuesta de los servicios dependerá de la localización física donde se necesiten.
       </Typography>
       <Grid container justifyContent="center" spacing={2}>
         <Grid item>
-          <CreditCardIcon style={{fontSize: 50}} />
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <CreditCardIcon style={{ fontSize: 50 }} />
+            <Box
+              component="img"
+              sx={{
+                height: 200,
+                width: 'auto',
+                marginTop: 1,
+              }}
+              alt="Visa & Mastercard logo."
+              src="https://i.ebayimg.com/images/g/oLoAAOxyzHxRKY87/s-l1600.jpg"
+            />
+          </Box>
         </Grid>
         <Grid item>
-          <AccountBalanceIcon style={{fontSize: 50}} />
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+            <AccountBalanceIcon style={{ fontSize: 50 }} />
+            <Box
+              component="img"
+              sx={{
+                height: 100,
+                width: 200,
+                marginTop: 0,
+              }}
+              alt="Logo Banorte."
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Logo_de_Banorte.svg"
+            />
+            <Box
+              component="img"
+              sx={{
+                height: 100,
+                width: 200,
+                marginTop: -3,
+              }}
+              alt="Logo Santander."
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b8/Banco_Santander_Logotipo.svg"
+            />
+          </Box>
         </Grid>
       </Grid>
     </Container>
