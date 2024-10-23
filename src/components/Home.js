95c7774ca062923e53595624cf6efdb97003a98a
Edgar Sandoval Rodríguez <up210614@alumnos.upa.edu.mx>
2024-10-22 23:26:34 -0600

    iconos en inicio

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,59 +1,66 @@
 import React from 'react';
 import { Container, Typography, Box, Card, CardContent } from '@mui/material';
 import { styled } from '@mui/system';
+import SecurityIcon from '@mui/icons-material/Security';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import StarIcon from '@mui/icons-material/Star';
+import InfoIcon from '@mui/icons-material/Info';
 
 const ZoomCard = styled(Card)(({ theme }) => ({
-  transition: 'transform 0.3s',
+  transition: 'transform 0.3s, box-shadow 0.3s',
   '&:hover': {
     transform: 'scale(1.05)',
+    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.1)', // Añadir una sombra ligera al hover
   },
 }));
-
-
 
 const Home = () => {
   return (
     <Container>
       <Box my={4}>
-        <Typography variant="h4" gutterBottom>
-          About SecurePro
+        <Typography variant="h4" gutterBottom style={{textAlign: 'center'}}>
+          Sobre SecurePro
         </Typography>
 
-        <Box mb={2}> {/* Add margin bottom for spacing */}
+        <Box mb={2}> {/* Misión */}
           <ZoomCard>
-            <CardContent>
+            <CardContent style={{ display: 'flex', alignItems: 'center' }}>
+              <SecurityIcon style={{ fontSize: 40, marginRight: '16px' }} />
               <Typography variant="body1">
-                <strong>Mission:</strong> To provide top-tier cybersecurity services that ensure the protection of our clients' digital assets.
+                <strong>Misión:</strong> Brindar servicios de ciberseguridad de primer nivel que garanticen la protección de los activos digitales de nuestros clientes.
               </Typography>
             </CardContent>
           </ZoomCard>
         </Box>
 
-        <Box mb={2}> {/* Add margin bottom for spacing */}
+        <Box mb={2}> {/* Visión */}
           <ZoomCard>
-            <CardContent>
+            <CardContent style={{ display: 'flex', alignItems: 'center' }}>
+              <VisibilityIcon style={{ fontSize: 40, marginRight: '16px' }} />
               <Typography variant="body1">
-                <strong>Vision:</strong> To become the leading global provider of innovative cybersecurity solutions.
+                <strong>Visión:</strong> Convertirnos en el proveedor global líder de soluciones innovadoras de ciberseguridad.
               </Typography>
             </CardContent>
           </ZoomCard>
         </Box>
 
-        <Box mb={2}> {/* Add margin bottom for spacing */}
+        <Box mb={2}> {/* Valores */}
           <ZoomCard>
-            <CardContent>
+            <CardContent style={{ display: 'flex', alignItems: 'center' }}>
+              <StarIcon style={{ fontSize: 40, marginRight: '16px' }} />
               <Typography variant="body1">
-                <strong>Values:</strong> Integrity, Innovation, Excellence, Customer-Centricity.
+                <strong>Valores:</strong> Integridad, Innovación, Excelencia, Orientación al Cliente.
               </Typography>
             </CardContent>
           </ZoomCard>
         </Box>
 
-        <Box mb={2}> {/* Add margin bottom for spacing */}
+        <Box mb={2}> {/* Descripción de la empresa */}
           <ZoomCard>
-            <CardContent>
+            <CardContent style={{ display: 'flex', alignItems: 'center' }}>
+              <InfoIcon style={{ fontSize: 40, marginRight: '16px' }} />
               <Typography variant="body1">
-                <strong>Company Description:</strong> SecurePro is dedicated to safeguarding the digital infrastructure of businesses worldwide. Our expert team offers advanced security strategies tailored to meet the unique challenges of each client.
+                <strong>Descripción de la Empresa:</strong> SecurePro se dedica a salvaguardar la infraestructura digital de empresas en todo el mundo. Nuestro equipo de expertos ofrece estrategias avanzadas de seguridad adaptadas a los desafíos únicos de cada cliente.
               </Typography>
             </CardContent>
           </ZoomCard>
